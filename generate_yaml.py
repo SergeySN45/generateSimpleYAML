f1dfd065d73494338e1824f4f54f18978e900a06
Sergey Nesterov <sergeygrif@gmail.com>
2024-01-27 23:22:16 +0300

    scretch2

--- generate_yaml.py
+++ generate_yaml.py
@@ -1,3 +1,80 @@
+minionTemplate = "      master: somehost\n      minionName: {MINIONNAME}"
+params = [
+    ["group-1",
+     [
+         ["host1", 4, 32, ["127.0.0.10", "127.0.0.1", "255.255.255.0", "dns1", "dns2"],
+          [
+              ["vlanName1", True],
+              ["vlanName2", True]
+          ],
+          [
+              [890, "t1"],
+              [987, "t2"]
+          ],
+          minionTemplate],
+         ["host2", 4, 16, ["127.0.0.10", "127.0.0.1", "255.255.255.0", "dns1", "dns2"],
+          [
+              ["vlanName1", True],
+              ["vlanName2", False]
+          ],
+          [
+              [890, "t1"],
+              [987, "t2"]
+          ],
+          minionTemplate]
+     ]
+    ],
+    ["group-2",
+     [
+         ["host1",4,8,["127.0.0.10","127.0.0.1","255.255.255.0","dns1","dns2"],
+          [
+              ["vlanName1",True],
+              ["vlanName2",False]
+          ],
+          [
+              [890,"t1"],
+              [987,"t2"]
+          ],
+          minionTemplate],
+         ["host2", 4, 8, ["127.0.0.10", "127.0.0.1", "255.255.255.0", "dns1", "dns2"],
+          [
+              ["vlanName1", True],
+              ["vlanName2", False]
+          ],
+          [
+              [890, "t1"],
+              [987, "t2"]
+          ],
+          minionTemplate],
+         ["host3", 4, 8, ["127.0.0.10", "127.0.0.1", "255.255.255.0", "dns1", "dns2"],
+          [
+              ["vlanName1", True],
+              ["vlanName2", False]
+          ],
+          [
+              [890, "t1"],
+              [987, "t2"]
+          ],
+          minionTemplate]
+     ]
+    ]
+]
+templateVM = ("  - {NAME}:\n    " +
+              "cpu: {CPU}\n    " +
+              "memory: {MEM}\n    " +
+              "script_args: {ARGS}\n    " +
+              "network:{NET}\n    " +
+              "disks:{DISKS}\n    " +
+              "minion: {MINION}")
+def createInterfaces(InterfaceParams):
+    interfaces = "\n"
+    i = 1
+    for interfaceParam in InterfaceParams:
+        interfase = "    - interface" + str(i) + ":\n      vlan: {VLANNAME}\n      CONNECTED: {INSTATE}\n"
+        interfaces = interfaces + interfase.format(VLANNAME=interfaceParam[0], INSTATE=interfaceParam[1])
+        i += 1
+    interfaces = interfaces[:-1]
+    return interfaces
 def createDisks(diskParams):
     disks = "\n"
     i = 1;
@@ -9,9 +86,11 @@ def createDisks(diskParams):
     return disks
 
 def createVM(template, params):
-    disks = createDisks(params[6])
-    minion = "\n" + params[7].format(MINIONNAME=params[0])
-    textfile = template.format(NAME=params[0],CPU=params[1],MEM=params[2],NET=params[3],DISKS=disks,MINION=minion) + "\n"
+    script_args = " ".join(params[3])
+    interfaces = createInterfaces(params[4])
+    disks = createDisks(params[5])
+    minion = "\n" + params[6].format(MINIONNAME=params[0])
+    textfile = template.format(NAME=params[0],CPU=params[1],MEM=params[2],ARGS=script_args,NET=interfaces,DISKS=disks,MINION=minion) + "\n"
     return textfile
 
 def createVMs(template,params):
@@ -21,13 +100,9 @@ def createVMs(template,params):
     textfile = textfile[:-1]
     return textfile
 
-minionTemplate = '''      master: somehost
-      minionName: {MINIONNAME}'''
-params = [["host1",4,8,123,321,"ttt",[[890,"t1"],[987,"t2"]],minionTemplate],["host2",8,16,456,876,"ttt",[[852,"t1"],[951,"t3"]],minionTemplate]]
-tempalteVMS = "{VMs}"
-templateVM = "- {NAME}:\n    cpu: {CPU}\n    memory: {MEM}\n    network: {NET}\n    disks:{DISKS}\n    minion: {MINION}"
-template2 = '''its another 
-  text'''
-
-finalvm = "mainTemplate:\n"  + createVMs(templateVM, params)
-print(finalvm)
+for groupVM in params:
+    finalvm = groupVM[0] + ":\n" + createVMs(templateVM, groupVM[1])
+    print(finalvm)
+    f = open(groupVM[0] + '.txt','w')  # открытие в режиме записи
+    f.write(finalvm)  # запись Hello World в файл
+    f.close()  # закрытие файла
